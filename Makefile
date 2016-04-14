789c10155e0a52ef7cf40b89d31627b0f948ae9d
Jascha Ehrenreich <jascha@jaeh.at>
2016-04-14 20:43:06 +0200

    add config and build files

---
+++ Makefile
@@ -0,0 +1,51 @@
+CLI=./cli.sh
+
+.PHONY: \
+	all \
+	dev \
+	build \
+	run \
+	debug \
+	logs \
+	rm \
+	stop \
+	backup \
+	help
+
+
+# TASKS
+
+all: help
+
+dev: run logs
+
+build:
+	@${CLI} $@
+
+run:
+	@${CLI} $@
+
+debug:
+	@${CLI} $@
+
+logs:
+	@${CLI} $@
+
+rm:
+	@${CLI} $@
+
+stop:
+	@${CLI} $@
+
+ip:
+	@${CLI} $@
+
+update:
+	@${CLI} $@
+
+status:
+	@${CLI} $@
+
+# help output
+help:
+	@${CLI} $@
---
+++ cli.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+source ./ENV.sh
+source ../../bin/tasks.sh
+
+echo "container: $CONTAINER_NAME"
+
+function build {
+  echo-start "build"
+
+  docker build \
+  --tag=$CONTAINER_NAME \
+  . # dot!
+
+  echo-finished "build"
+}
+
+function run() {
+  remove
+
+  echo-start "run"
+
+  docker run \
+    --detach \
+    --name ${CONTAINER_NAME} \
+    --volume $DATA_DIR/postfix/data:/home/postfix/ \
+    $CONTAINER_NAME
+
+  ip
+
+  echo-finished "run"
+}
+
+function help() {
+  echo "Container: $CONTAINER_NAME"
+  echo ""
+  echo "Usage:"
+  echo ""
+  echo './cli.sh $command'
+  echo ""
+  echo "commands:"
+  echo "build  - build docker container"
+  echo "run    - run docker container"
+  echo "remove - remove container"
+  echo "logs   - tail the container logs"
+  echo "debug  - connect to container debug session"
+  echo "stop   - stop container"
+  echo "help   - this help text"
+}
+
+if [ $1 ]
+then
+  function=$1
+  shift
+  $function $@
+else
+  help $@
+fi
